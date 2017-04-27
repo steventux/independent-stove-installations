7fc1d4382c66c0cbe43f3ce1b72a7dd43816cca2
Steve Laing <steve.laing@gmail.com>
2017-04-27 17:01:21 +0100

    Initial Keystone project

---
+++ keystone.js
@@ -0,0 +1,61 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+var handlebars = require('express-handlebars');
+
+keystone.init({
+	'name': 'independent-stove-installations',
+	'brand': 'independent-stove-installations',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': '.hbs',
+
+	'custom engine': handlebars.create({
+		layoutsDir: 'templates/views/layouts',
+		partialsDir: 'templates/views/partials',
+		defaultLayout: 'default',
+		helpers: new require('./templates/views/helpers')(),
+		extname: '.hbs',
+	}).engine,
+
+	'emails': 'templates/emails',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+
+if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+	console.log('----------------------------------------'
+	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
+	+ '\n----------------------------------------'
+	+ '\nYou have opted into email sending but have not provided'
+	+ '\nmailgun credentials. Attempts to send will fail.'
+	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
+	+ '\nset up your mailgun integration');
+}
+
+
+keystone.start();
